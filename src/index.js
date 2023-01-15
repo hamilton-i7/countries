9a17ed2fe6ad2385700eb9ece437bcf17a16ad3c
Juan Hamilton <hedwards507@gmail.com>
2023-01-15 14:34:51 -0500

    Upate React

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom';
-import App, { HOME_PATH } from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import { HashRouter as Router } from 'react-router-dom'
+import App, { HOME_PATH } from './App'
+import reportWebVitals from './reportWebVitals'
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.render(
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
+
+root.render(
   <React.StrictMode>
     <Router basename={HOME_PATH}>
       <App />
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
